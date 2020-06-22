8227e74bf826b815bb688b9dedee574cbce7a15c
chriswhays <chriswhays@gmail.com>
2020-06-22 10:12:39 -0400

    Updated H Matrix Generation

--- MeasurementModels/simple.py
+++ MeasurementModels/simple.py
@@ -11,7 +11,7 @@ import random
 class MeasurementModel():
  
     Q = np.zeros(6) # Process Noise
-    H = np.identity(6) # Measurement Model Jacobian
+
         
     std_dev = 0.0001
     R = np.identity(6)*std_dev
@@ -19,3 +19,9 @@ class MeasurementModel():
     @classmethod
     def MeasureFcn(cls, state_real):
         return np.random.multivariate_normal(state_real, cls.R)
+
+    @classmethod
+    def BuildMeasureJacob(cls,state_real):
+        H = np.identity(6)
+
+        return H # Measurement Model Jacobian
