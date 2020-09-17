c46375f4d471bb71787e491e82618cf2d874348e
chriswhays <chriswhays@gmail.com>
2020-09-17 19:59:43 -0400

    Added in noise levels

---
+++ MeasurementModels/full_state_high_noise.py
@@ -0,0 +1,31 @@
+"""
+
+
+"""
+
+import numpy as np 
+import random 
+
+class MeasurementModel():
+ 
+    Q = np.zeros(6) # Process Noise
+
+        
+    std_dev = 1000# Noi measurement noise
+    R = np.identity(6)*std_dev # Measurement NOise
+
+    dimension = 6
+
+    @classmethod
+    def MeasureFcn(cls, state_real):
+        return np.random.multivariate_normal(state_real, cls.R)
+
+    @classmethod
+    def h(cls, state_real):
+        return np.reshape(state_real, [6,1])
+
+    @classmethod
+    def BuildMeasureJacob(cls,state_real):
+        H = np.identity(6)
+
+        return H # Measurement Model Jacobian
---
+++ MeasurementModels/full_state_low_noise.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+
+"""
+
+import numpy as np 
+import random 
+
+class MeasurementModel():
+ 
+    Q = np.zeros(6) # Process Noise
+
+        
+    std_dev = 0.1# Noi measurement noise
+    R = np.identity(6)*std_dev # Measurement NOise
+
+    dimension = 6
+
+    @classmethod
+    def MeasureFcn(cls, state_real):
+        return np.random.multivariate_normal(state_real, cls.R)
+
+    @classmethod
+    def h(cls, state_real):
+        return np.reshape(state_real, [6,1])
+
+    @classmethod
+    def BuildMeasureJacob(cls,state_real):
+        H = np.identity(6)
+
+        return H # Measurement Model Jacobian
